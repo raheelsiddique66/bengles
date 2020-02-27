87162e4030c27854963d5c03c68c7e3c40077120
mac <1>
2020-02-28 01:53:07 +0500

    import script

--- import.php
+++ import.php
@@ -11,40 +11,38 @@ if (($handle = fopen("data.csv", "r")) !== FALSE) {
         $customer_id = get_table_id("customer", "customer_name", $data[1]);
         $color_id = get_table_id("color", "title", $data[3]);
         $design_id = get_table_id("design", "title", $data[10]);
-        if( $data[2] == "RECEVIED" ){
-            $table = "incoming";
-        }
-        else if( $data[2] == "WASHING" ){
-            $table = "washing";
-        }
-        else if( $data[2] == "SENT" ){
-            $table = "delivery";
-        }
-        $ch = doquery("select * from ".$table." where date='".date("Y-m-d", strtotime($data[0]))."' and customer_id = '".$customer_id."'", $dblink);
-        if( numrows($ch) > 0 ){
-            $ch = dofetch($ch);
-            $id = $ch["id"];
-        }
-        else {
+        if(!empty($customer_id) && !empty($color_id) && !empty($design_id)) {
             if ($data[2] == "RECEVIED") {
-                doquery("insert into incoming(gatepass_id, date, customer_id, labour_id) values('" . slash(trim($data[11])) . "', '" . date("Y-m-d", strtotime($data[0])) . "', '" . $customer_id . "', 0)", $dblink);
+                $table = "incoming";
             } else if ($data[2] == "WASHING") {
-                doquery("insert into washing(date, customer_id) values('" . date("Y-m-d", strtotime($data[0])) . "', '" . $customer_id . "')", $dblink);
+                $table = "washing";
             } else if ($data[2] == "SENT") {
-                doquery("insert into delivery(gatepass_id, date, customer_id, claim, labour_id) values('" . slash(trim($data[11])) . "', '" . date("Y-m-d", strtotime($data[0])) . "', '" . $customer_id . "', 0, 0)", $dblink);
+                $table = "delivery";
             }
-            $id = inserted_id();
-        }
-        for($i = 12; $i <= 16; $i++){
-            if( !empty($data[$i]) ){
-                if( $data[2] == "RECEVIED" ) {
-                    doquery("insert into incoming_items(incoming_id, machine_id, color_id, size_id, design_id, quantity) values('".$id."', '0', '".$color_id."', '".($i-11)."', '".$design_id."', '".$data[$i]."')", $dblink);
+            $ch = doquery("select * from " . $table . " where date='" . date("Y-m-d", strtotime($data[0])) . "' and customer_id = '" . $customer_id . "'", $dblink);
+            if (numrows($ch) > 0) {
+                $ch = dofetch($ch);
+                $id = $ch["id"];
+            } else {
+                if ($data[2] == "RECEVIED") {
+                    doquery("insert into incoming(gatepass_id, date, customer_id, labour_id) values('" . slash(trim($data[11])) . "', '" . date("Y-m-d", strtotime($data[0])) . "', '" . $customer_id . "', 0)", $dblink);
+                } else if ($data[2] == "WASHING") {
+                    doquery("insert into washing(date, customer_id) values('" . date("Y-m-d", strtotime($data[0])) . "', '" . $customer_id . "')", $dblink);
+                } else if ($data[2] == "SENT") {
+                    doquery("insert into delivery(gatepass_id, date, customer_id, claim, labour_id) values('" . slash(trim($data[11])) . "', '" . date("Y-m-d", strtotime($data[0])) . "', '" . $customer_id . "', 0, 0)", $dblink);
                 }
-                else if( $data[2] == "WASHING" ) {
-                    doquery("insert into washing_items(washing_id, color_id, size_id, design_id, quantity) values('".$id."', '".$color_id."', '".($i-11)."', '".$design_id."', '".$data[$i]."')", $dblink);
-                }
-                else if( $data[2] == "SENT" ) {
-                    doquery("insert into delivery_items(delivery_id, machine_id, color_id, size_id, design_id, quantity, extra, unit_price) values('".$id."', '0', '".$color_id."', '".($i-11)."', '".$design_id."', '".$data[$i]."', '".$data[7]."', '".$data[8]."')", $dblink);
+                $id = inserted_id();
+            }
+            for ($i = 12; $i <= 16; $i++) {
+                $data[$i] = trim($data[$i]);
+                if (!empty($data[$i])) {
+                    if ($data[2] == "RECEVIED") {
+                        doquery("insert into incoming_items(incoming_id, machine_id, color_id, size_id, design_id, quantity) values('" . $id . "', '0', '" . $color_id . "', '" . ($i - 11) . "', '" . $design_id . "', '" . $data[$i] . "')", $dblink);
+                    } else if ($data[2] == "WASHING") {
+                        doquery("insert into washing_items(washing_id, color_id, size_id, design_id, quantity) values('" . $id . "', '" . $color_id . "', '" . ($i - 11) . "', '" . $design_id . "', '" . $data[$i] . "')", $dblink);
+                    } else if ($data[2] == "SENT") {
+                        doquery("insert into delivery_items(delivery_id, machine_id, color_id, size_id, design_id, quantity, extra, unit_price) values('" . $id . "', '0', '" . $color_id . "', '" . ($i - 11) . "', '" . $design_id . "', '" . $data[$i] . "', '" . (int)$data[7] . "', '" . (int)$data[8] . "')", $dblink);
+                    }
                 }
             }
         }
