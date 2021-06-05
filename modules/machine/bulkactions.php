dc7973c64141521adf8bd110d0fd88227c92f26c
Raheel <s.raheelshaikh@gmail.com>
2021-06-05 19:06:57 +0500

    payment

--- modules/machine/bulkactions.php
+++ modules/machine/bulkactions.php
@@ -15,7 +15,7 @@ if(isset($_GET["action"]) && $_GET["action"]!=""){
 		if($bulk_action=="delete"){
 			$i=0;
 			while($i<count($id)){
-				doquery("delete from color where id='".$id[$i]."'",$dblink);
+				doquery("delete from machine where id='".$id[$i]."'",$dblink);
 				$i++;
 			}
 			header("Location: machine_manage.php?tab=list&msg=".url_encode("Records Deleted."));
@@ -24,7 +24,7 @@ if(isset($_GET["action"]) && $_GET["action"]!=""){
 		if($bulk_action=="statuson"){
 			$i=0;
 			while($i<count($id)){
-				doquery("update color set status=1 where id='".$id[$i]."'",$dblink);
+				doquery("update machine set status=1 where id='".$id[$i]."'",$dblink);
 				$i++;
 			}
 			header("Location: machine_manage.php?tab=list&msg=".url_encode("Records Status On."));
@@ -33,7 +33,7 @@ if(isset($_GET["action"]) && $_GET["action"]!=""){
 		if($bulk_action=="statusof"){
 			$i=0;
 			while($i<count($id)){
-				doquery("update color set status=0 where id='".$id[$i]."'",$dblink);
+				doquery("update machine set status=0 where id='".$id[$i]."'",$dblink);
 				$i++;
 			}
 			header("Location: machine_manage.php?tab=list&msg=".url_encode("Records Status Off."));
