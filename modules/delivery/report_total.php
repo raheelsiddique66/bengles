025012ee9262dd069923399efb50f6a0ba5c2809
raheelsiddique66 <41738172+raheelsiddique66@users.noreply.github.com>
2021-12-08 19:58:44 +0500

    total rep

--- modules/delivery/report_total.php
+++ modules/delivery/report_total.php
@@ -130,7 +130,7 @@ if( numrows( $rs ) > 0 ) {
     $cus_balance = 0;
 	while( $r = dofetch( $rs ) ) {
 	    $balance = get_customer_balance($r["customerid"], date_dbconvert($date_from));
-        $sql="select sum(amount) as amount, sum(discount) as discount, sum(claim) as claim from customer_payment where customer_id = '".$r[ "customerid" ]."'".(!empty($machine_id)?" and machine_id = '".$machine_id."'":"")." and datetime_added>='".date_dbconvert($date_from)." 00:00:00' and datetime_added<='".date_dbconvert($date_to)." 23:59:59'";
+        $sql="select sum(amount) as amount, sum(discount) as discount, sum(claim) as claim from customer_payment where customer_id = '".$r[ "customerid" ]."'".(!empty($machine_id)?" and machine_id = '".$machine_id."'":"")." and datetime_added>='".date_dbconvert($date_from)."' and datetime_added<='".date_dbconvert($date_to)."'";
         $income1=dofetch(doquery($sql,$dblink));
         $income = $income1[ "amount" ];
         $discount = $income1[ "discount" ];
