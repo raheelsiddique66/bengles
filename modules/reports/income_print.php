01f936323685a5246175db54202dd07ad3d2f05f
raheelsiddique66 <41738172+raheelsiddique66@users.noreply.github.com>
2021-12-23 19:51:02 +0500

    income and print

--- modules/reports/income_print.php
+++ modules/reports/income_print.php
@@ -98,13 +98,13 @@ table {
         //$q=dofetch(doquery("select sum(quantity) as quantity from delivery a left join delivery_items b on a.id = b.delivery_id where status = 1 and date>='".date('Y-m-d',strtotime(date_dbconvert($date_from)))."' and date<='".date('Y-m-d',strtotime(date_dbconvert($date_to)))."'",$dblink));
 		//$payment_total += $unit_price*$q["quantity"];
 	?>
-    <?php if($_SERVER['SERVER_NAME'] != 'star.burhanpk.com'){?>
+    <?php if($_SERVER['SERVER_NAME'] != 'star.burhanpk.com' || $_SERVER['SERVER_NAME'] != 'goldenglass.burhanpk.com'){?>
     <tr>
         <th align="right">Income from <?php echo $date_from?> to <?php echo $date_to?></th>
         <th align="right">Rs. <?php echo curr_format($payment_total)?></th>
     </tr>
     <?php }?>
-    <?php if($_SERVER['SERVER_NAME'] != 'star.burhanpk.com'){?>
+    <?php if($_SERVER['SERVER_NAME'] != 'star.burhanpk.com' || $_SERVER['SERVER_NAME'] != 'goldenglass.burhanpk.com'){?>
     <tr>
         <th align="right">Salary from <?php echo $date_from?> to <?php echo $date_to?></th>
         <th align="right">Rs. <?php
@@ -135,7 +135,7 @@ table {
         <th align="right">Total Expense</th>
         <th align="right">Rs. <?php echo curr_format($total+$salary_total)?></th>
     </tr>
-    <?php if($_SERVER['SERVER_NAME'] != 'star.burhanpk.com'){?>
+    <?php if($_SERVER['SERVER_NAME'] != 'star.burhanpk.com' || $_SERVER['SERVER_NAME'] != 'goldenglass.burhanpk.com'){?>
     <tr>
         <th align="right">Net Income</th>
         <th align="right">Rs. <?php echo curr_format($payment_total-$salary_total-$total)?></th>
